2d735cdee2d65d3d81976e98fb3ba30876ef9e31
David Nguyen <90191809+davidnguyen7@users.noreply.github.com>
2024-02-08 14:51:38 +1100

    add Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import {Inter} from 'next/font/google';
 import './globals.css';
 import PageLink from './PageLink';
 import {Analytics} from '@vercel/analytics/react';
+import {SpeedInsights} from '@vercel/speed-insights/next';
 
 const inter = Inter({subsets: ['latin']});
 
@@ -44,6 +45,7 @@ export default function RootLayout({
           {children}
         </div>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
